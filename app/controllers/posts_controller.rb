b56eb9b10cd0da55c3f35db9fcf63a7c9c89c560
Eamon <eamontaaffe@gmail.com>
2015-12-12 14:27:30 +1100

    feat: Validation
    Added validation to post controller and error messages to the new form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,17 @@ class PostsController < ApplicationController
 		@posts = Post.all.order('created_at DESC')
 	end
 	
-	def new	
+	def new
+		@post = Post.new	
 	end
 
 	def create
 		@post = Post.new(post_params)	
-		@post.save
-
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show
@@ -20,6 +23,6 @@ class PostsController < ApplicationController
 	private 
 
 	def post_params
-		params.require(:post).permit(:title)
+		params.require(:post).permit(:title,:body)
 	end
 end
